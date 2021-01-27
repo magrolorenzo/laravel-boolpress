97f0b9258edb0fe44d952fda383a73e66a5f7010
Lorenzo Magro <magrolorenzo@gmail.com>
2021-01-27 18:15:14 +0100

    Aggiunte relazioni nei Model Post e Category

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
+    public function category(){
+        return $this->belongsTo("App\Category");
+    }
+
     public $fillable = [
         'author',
         'title',
