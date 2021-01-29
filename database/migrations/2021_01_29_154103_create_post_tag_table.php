99ede8eb32118d3a6afde1ba68ceeff9e0586a22
Lorenzo Magro <magrolorenzo@gmail.com>
2021-01-29 16:56:25 +0100

    Creata tabella ponte Post Tag

--- database/migrations/2021_01_29_154103_create_post_tag_table.php
+++ database/migrations/2021_01_29_154103_create_post_tag_table.php
@@ -14,7 +14,11 @@ class CreatePostTagTable extends Migration
     public function up()
     {
         Schema::create('post_tag', function (Blueprint $table) {
-            $table->id();
+            $table->unsignedBigInteger("post_id");
+            $table->foreign("post_id")->references("id")->on("posts");
+            $table->unsignedBigInteger("tag_id");
+            $table->foreign("tag_id")->references("id")->on("tags");
+            $table->primary(["post_id","tag_id"]);
             $table->timestamps();
         });
     }
